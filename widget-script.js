e941329d0f0d8907d54f11bb03b354b4fa444936
dirkcallaway <35826733+dirkcallaway@users.noreply.github.com>
2019-08-12 06:47:36 -0600

    Initial upload

---
+++ widget-script.js
@@ -0,0 +1,30 @@
+const divContainer = document.createElement('div')
+const frameBox = document.createElement('iframe')
+frameBox.setAttribute("src", "https://pponboardingembeded.herokuapp.com/")
+frameBox.setAttribute("scrolling", "no")
+frameBox.setAttribute("id", "onboardwidget")
+frameBox.style.border = 'none'
+frameBox.style.width = '450px'
+divContainer.appendChild(frameBox)
+divContainer.style.zIndex = '2'
+divContainer.style.position = 'absolute'
+divContainer.style.top = '40px'
+divContainer.style.right = '0px'
+
+
+
+window.onload = function () {
+    document.getElementById("widget-box").appendChild(divContainer)
+    let checklistOpened = false;
+    const openBtn = document.getElementById('onboard-btn')
+    const frame = document.getElementById('onboardwidget').contentWindow
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        console.log("Button Pushed!")
+        
+        frame.postMessage(OnBoard_config.userVID, 'https://pponboardingembeded.herokuapp.com/')
+
+    }
+    openBtn.addEventListener("click", sendMessage)
+}
